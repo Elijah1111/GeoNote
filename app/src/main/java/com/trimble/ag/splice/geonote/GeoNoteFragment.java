d12d10b1d71dbfd6b49ff029402fde8146a60920
jacks0nwill <jwill1@mymail.mines.edu>
2021-06-07 12:28:27 -0600

    Testing map implementation

--- app/src/main/java/com/trimble/ag/splice/geonote/GeoNoteFragment.java
+++ app/src/main/java/com/trimble/ag/splice/geonote/GeoNoteFragment.java
@@ -2,17 +2,30 @@ package com.trimble.ag.splice.geonote;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.trimble.ag.splice.Extension;
 import com.trimble.ag.toolkit.ui.SpliceFragment;
 
-public class GeoNoteFragment extends SpliceFragment {
+public class GeoNoteFragment extends SpliceFragment implements OnMapReadyCallback{
+    private static final String TAG = "GeoNoteFragment";
 
     public GeoNoteFragment() {
         // Required empty public constructor
@@ -23,15 +36,35 @@ public class GeoNoteFragment extends SpliceFragment {
     }
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return getLayoutInflater(inflater).inflate(R.layout.geonote, container, false);
+        View view= getLayoutInflater(inflater).inflate(R.layout.geonote, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.main_branch_map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng UCA = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(UCA).title("YOUR TITLE")).showInfoWindow();
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCA,17));
+
     }
 }
+
+
+    /*public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return getLayoutInflater(inflater).inflate(R.layout.geonote, container, false);
+    }*/
