8e172612682391c110557a85aefdfa8e7aecb277
jacks0nwill <jwill1@mymail.mines.edu>
2021-06-08 10:07:04 -0600

    More maps testing

--- app/src/main/java/com/trimble/ag/splice/geonote/GeoNoteFragment.java
+++ app/src/main/java/com/trimble/ag/splice/geonote/GeoNoteFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -24,7 +25,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.trimble.ag.splice.Extension;
 import com.trimble.ag.toolkit.ui.SpliceFragment;
 
-public class GeoNoteFragment extends SpliceFragment implements OnMapReadyCallback{
+public class GeoNoteFragment extends SpliceFragment{
     private static final String TAG = "GeoNoteFragment";
 
     public GeoNoteFragment() {
@@ -35,30 +36,74 @@ public class GeoNoteFragment extends SpliceFragment implements OnMapReadyCallbac
         super(extension);
     }
 
-
-    private GoogleMap mMap;
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= getLayoutInflater(inflater).inflate(R.layout.geonote, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.main_branch_map);
-        mapFragment.getMapAsync(this);
-        return view;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = getLayoutInflater(inflater).inflate(R.layout.geonote, container, false);
 
+        mMapView = (MapView) rootView.findViewById(R.id.mapview);
 
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+        return rootView;
+    }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng UCA = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(UCA).title("YOUR TITLE")).showInfoWindow();
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
 
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCA,17));
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
 
