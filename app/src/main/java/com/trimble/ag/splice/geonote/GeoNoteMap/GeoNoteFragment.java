2c7aaf0eb7ff95069637186410b6ebff54f297bc
jacks0nwill <jwill1@mymail.mines.edu>
2021-06-10 18:08:11 -0600

    Starting commit

--- app/src/main/java/com/trimble/ag/splice/geonote/GeoNoteMap/GeoNoteFragment.java
+++ app/src/main/java/com/trimble/ag/splice/geonote/GeoNoteMap/GeoNoteFragment.java
@@ -1,28 +1,22 @@
-package com.trimble.ag.splice.geonote;
+package com.trimble.ag.splice.geonote.GeoNoteMap;
 
 import android.os.Bundle;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.trimble.ag.splice.Extension;
+import com.trimble.ag.splice.geonote.R;
 import com.trimble.ag.toolkit.ui.SpliceFragment;
 
 public class GeoNoteFragment extends SpliceFragment{
@@ -48,8 +42,7 @@ public class GeoNoteFragment extends SpliceFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = getLayoutInflater(inflater).inflate(R.layout.geonote, container, false);
-
-        mMapView = (MapView) rootView.findViewById(R.id.mapview);
+        mMapView =(MapView) rootView.findViewById(R.id.mapview);
 
         mMapView.onCreate(savedInstanceState);
 
@@ -78,6 +71,7 @@ public class GeoNoteFragment extends SpliceFragment{
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
+
 
         return rootView;
     }
