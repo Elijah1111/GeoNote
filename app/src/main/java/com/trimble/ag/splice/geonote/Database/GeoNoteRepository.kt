0c6dffb9e1710f846eebb9994a777c9bace6f175
jacks0nwill <jwill1@mymail.mines.edu>
2021-06-13 14:07:20 -0600

    Camera Code: crash when click camera icon for now

--- app/src/main/java/com/trimble/ag/splice/geonote/Database/GeoNoteRepository.kt
+++ app/src/main/java/com/trimble/ag/splice/geonote/Database/GeoNoteRepository.kt
@@ -3,12 +3,14 @@ package com.trimble.ag.splice.geonote.Database
 import android.content.Context
 import androidx.lifecycle.LiveData
 import com.trimble.ag.splice.geonote.GeoNote
+import java.io.File
 import java.util.*
 import java.util.concurrent.Executors
 
-class GeoNoteRepository private constructor (private val geoNoteDao: GeoNoteDao)
+class GeoNoteRepository private constructor (private val geoNoteDao: GeoNoteDao, context: Context)
 {
     private val executor = Executors.newSingleThreadExecutor()
+    private val filesDir = context.applicationContext.filesDir
     fun getGeoNotes(): LiveData<List<GeoNote>> =geoNoteDao.getNotes()
     fun getGeoNote(id: UUID): LiveData<GeoNote?> = geoNoteDao.getNote(id)
     fun updateGeoNote(geoNote: GeoNote){
@@ -33,11 +35,12 @@ class GeoNoteRepository private constructor (private val geoNoteDao: GeoNoteDao)
                 var instance = INSTANCE
                 if(instance == null) {
                     val database = GeoNoteDatabase.getInstance(context)
-                    instance = GeoNoteRepository(database.geoNoteDao)
+                    instance = GeoNoteRepository(database.geoNoteDao, context)
                     INSTANCE = instance
                 }
                 return instance
             }
         }
     }
+    fun getPhotoFile(geoNote: GeoNote):File = File(filesDir, geoNote.photoFileName)
 }
