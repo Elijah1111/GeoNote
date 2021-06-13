bd1c842a67b73ea50290592067f041f2be0e9063
jacks0nwill <jwill1@mymail.mines.edu>
2021-06-13 12:02:52 -0600

    Added GeoNote stuff

--- app/src/test/java/com/trimble/ag/splice/geonote/GeoNoteTests.java
+++ app/src/test/java/com/trimble/ag/splice/geonote/GeoNoteTests.java
@@ -10,9 +10,9 @@ public class GeoNoteTests {
     GeoNote note = new GeoNote("Note",R.drawable.crop,GeoNoteType.ANIMAL,5.4,6.2);
     @Test
     public void NameTests() {
-        assertEquals("Note",note.name);
-        note.name = "Cows";
-        assertEquals("Cows",note.name);
+        assertEquals("Note",note.getName());
+        note.setName("Cows");
+        assertEquals("Cows",note.getName());
     }
     @Test
     public void UUIDTest(){
@@ -23,41 +23,40 @@ public class GeoNoteTests {
         for(int i=0; i<100; i++){
             for(int j=0; j<100; j++){
                 if(i==j) {
-                    assertEquals(notes[i].uid, notes[j].uid);//verify its own uid is equal to its self
+                    assertEquals(notes[i].getUid(), notes[j].getUid());//verify its own uid is equal to its self
                     continue;
                 }
-                assertNotEquals(notes[i].uid,notes[j].uid);//verify UUIDs are unique
+                assertNotEquals(notes[i].getUid(),notes[j].getUid());//verify UUIDs are unique
             }
         }
     }
     @Test
     public void PictureTest() {
         String path1="/app/media/pic1.jpg", path2="/app/media/pic2.jpg";
-        note.addImages(path1,path2);
-        String[] images = note.pictures;
-        assertEquals(path1,images[0]);
-        assertEquals(path2,images[1]);
+        note.setPictures(path1);
+        String images = note.getPictures();
+        assertEquals(path1,images);
     }
     @Test
     public void AudioTest() {
         String path="/app/media/audio.mp4";
-        note.addAudio(path);
-        assertEquals(path,note.audio);
+        note.setAudio(path);
+        assertEquals(path,note.getAudio());
     }
     @Test
     public void IconTest(){
-        assertEquals(R.drawable.crop,note.icon);
+        assertEquals(R.drawable.crop,note.getIcon());
     }
     @Test
     public void TypeTest(){
-        assertEquals(GeoNoteType.ANIMAL,note.type);
-        note.type = GeoNoteType.EQUIPMENT;
-        assertNotEquals(GeoNoteType.ANIMAL,note.type);
-        assertEquals(GeoNoteType.EQUIPMENT,note.type);
+        assertEquals(GeoNoteType.ANIMAL,note.getType());
+        note.setType( GeoNoteType.EQUIPMENT);
+        assertNotEquals(GeoNoteType.ANIMAL,note.getType());
+        assertEquals(GeoNoteType.EQUIPMENT,note.getType());
     }
     @Test
     public void PosTest(){
-        double[] pos = note.pos;
+        double[] pos = note.getPos();
         assertEquals(5.4,pos[0],0.00001);
         assertEquals(6.2,pos[1],0.00001);
     }
